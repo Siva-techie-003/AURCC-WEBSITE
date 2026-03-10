1091fe700c63bb4c2ca8adc03a2ff50b1fd86ad3
Santhosh-6 <darksantho@gmail.com>
2026-03-10 23:11:47 +0530

    c&s

--- client/src/views/CurriculumSyllabus.jsx
+++ client/src/views/CurriculumSyllabus.jsx
@@ -4,6 +4,17 @@ import './CurriculumSyllabus.css';
 
 const CurriculumSyllabus = () => {
 
+    const [data, setData] = useState(null);
+
+useEffect(() => {
+fetch("http://localhost:5000/api/curriculum-syllabus")
+.then(res => res.json())
+.then(resData => {
+setData(resData);
+})
+.catch(err => console.error(err));
+}, []);
+
 const SectionHeader = ({ title }) => (
 <header className="flex flex-col md:flex-row md:items-end justify-between border-b-4 border-[rgb(100,25,25)] pb-8 mb-12 gap-6 animate-fadeInUp">
 <div className="max-w-xl">
@@ -12,19 +23,20 @@ const SectionHeader = ({ title }) => (
 {title}
 </h2>
 
+
 </div>
 </header>
 );
 
 return (
 
-<div className="flex-grow bg-gray-50 min-h-screen text-left">
+<div className="flex-grow bg-gray-50 min-h-screen text-left pt-[120px] sm:pt-[140px] lg:pt-[120px]">
 
 {/* HERO */}
 <section className="relative w-full h-56 sm:h-72 md:h-96 lg:h-[50vh] flex items-center justify-center overflow-hidden">
 
 <img
-src="/syllabus.webp"
+src="http://localhost:5000/syllabus.webp"
 alt="Curriculum & Syllabus"
 className="absolute inset-0 w-full h-full object-cover object-center"
 />
